1974c5a9e7844a706e746fc9b7fcf1fb90e6597f
hs <1519706548@qq.com>
2026-04-17 23:39:31 +0800

    feat: add RequestParam

--- 02-springboot-mini-loops/src/main/java/com/hs/sbminiloops/controller/HelloController.java
+++ 02-springboot-mini-loops/src/main/java/com/hs/sbminiloops/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.hs.sbminiloops.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello 嘿嘿";
+    public String hello(@RequestParam String name){
+        return "hello 嘿嘿," + name;
     }
 
     @GetMapping("/bye")
