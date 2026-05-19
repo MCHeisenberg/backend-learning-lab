837bd92f66b85f441e5101dfa85eee71c117d923
hs <1519706548@qq.com>
2026-05-19 19:03:02 +0800

    finish phase3 block02 verify mysql datasource connection

---
+++ 02-springboot-mini-loops/src/main/java/com/hs/sbminiloops/config/DbConnectionTestRunner.java
@@ -0,0 +1,23 @@
+package com.hs.sbminiloops.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+public class DbConnectionTestRunner implements CommandLineRunner {
+    private final DataSource dataSource;
+
+    public DbConnectionTestRunner(DataSource dataSource){
+        this.dataSource=dataSource;
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        try(Connection connection=dataSource.getConnection()){
+            System.out.println("MySQL connected ok, database="+connection.getCatalog());
+        }
+    }
+}
